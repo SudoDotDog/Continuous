f23f5326fe89b8ddb44fc70d4a6dc9eb1781fe41
WMXPY <wm@mengw.io>
2019-05-30 21:32:10 -0500

    implement cached class

--- src/cached.ts
+++ src/cached.ts
@@ -8,15 +8,44 @@ import { Awaitable } from "./declare";
 
 export class Cached<T = any> {
 
-    public static create<T = any>(fetch: Awaitable<T>): Cached<T> {
+    public static create<T = any>(fetch: Awaitable<T>, timeout: number = 50000): Cached<T> {
 
-        return new Cached<T>(fetch);
+        return new Cached<T>(fetch, timeout);
     }
 
     private readonly _fetch: Awaitable<T>;
+    private readonly _timeout: number;
 
-    private constructor(fetch: Awaitable<T>) {
+    private _cached?: T;
+    private _clearer?: any;
+
+    private constructor(fetch: Awaitable<T>, timeout: number) {
 
         this._fetch = fetch;
+        this._timeout = timeout;
+    }
+
+    public async get(): Promise<T> {
+
+        if (this._cached) {
+            return this._cached;
+        }
+
+        const newValue: T = await this.update();
+        return newValue;
+    }
+
+    public async update(): Promise<T> {
+
+        clearTimeout(this._clearer);
+        const newValue: T = await this._fetch();
+        this._cached = newValue;
+
+        this._clearer = setTimeout((): void => {
+            this._cached = undefined;
+            return;
+        }, this._timeout);
+
+        return newValue;
     }
 }
